59cb57184c8fae00d36c96a34e2cb2e6371ab35b
Bjoern Kaczmarek <bjoern.kaczmarek@gmail.com>
2020-02-21 18:20:41 +1100

    Initial Commit

---
+++ GuessingGameExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessingGameExercise
+//
+//  Created by Björn Kaczmarek on 21/2/20.
+//  Copyright © 2020 Björn Kaczmarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
